d8eaac060825b7c71e3b4f6c9af1e32ebb639de5
tourervit <tourervit@gmail.com>
2019-04-07 19:45:37 +0700

    Changed logic for props

---
+++ src/common/withOptions/PropsModal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Tooltip, Modal } from 'antd';
+
+export default ({ availableProps, propsModal, toggle }) => {
+  return (
+    <Modal
+      visible={propsModal}
+      onCancel={toggle}
+      footer={false}
+      bodyStyle={{
+        maxHeight: '80vh',
+        overflowY: 'scroll',
+        '-webkit-mask-image':
+          'linear-gradient(to bottom, transparent, black 4%, black 96%, transparent)',
+      }}
+      centered={true}
+    >
+      {Object.entries(availableProps).map(([category, props]) => (
+        <>
+          <h4 className="prop-category">{category}</h4>
+          <ul>
+            {Object.entries(props).map(([propName, propData]) => (
+              <li key={propName}>
+                <Tooltip title={propData.description} placement="left">
+                  <span className="prop-name">{propName}</span> :{' '}
+                </Tooltip>
+                <span className="prop-type">{propData.type}</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      ))}
+    </Modal>
+  );
+};
---
+++ src/common/withOptions/withOptions.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button, Icon, ButtonGroup } from 'antd';
+import PropsModal from './PropsModal';
+import HighlightedSourceCode from './HighlightedSourceCode';
+
+export default (Component, availableProps, code) =>
+  class WithOptions extends React.Component {
+    state = {
+      propsModal: false,
+      sourceCode: false,
+    };
+    toggle = value => {
+      this.setState({ [value]: !this.state[value] });
+    };
+
+    render() {
+      const { sourceCode, propsModal } = this.state;
+      return (
+        <>
+          <Component />
+          <section className="options">
+            <Button.Group>
+              <Button
+                onClick={() => this.toggle('propsModal')}
+                icon="setting"
+              />
+              <Button
+                onClick={() => this.toggle('sourceCode')}
+                type="primary"
+                icon="code"
+              />
+            </Button.Group>
+
+            <PropsModal
+              availableProps={availableProps}
+              propsModal={propsModal}
+              toggle={() => this.toggle('propsModal')}
+            />
+
+            {sourceCode && <HighlightedSourceCode code={code} />}
+          </section>
+        </>
+      );
+    }
+  };
