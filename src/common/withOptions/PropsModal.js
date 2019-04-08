eb979e015fdc3fe06f6672c900f86cce319f5c26
tourervit <tourervit@gmail.com>
2019-04-08 09:09:15 +0700

    Added keys

--- src/common/withOptions/PropsModal.js
+++ src/common/withOptions/PropsModal.js
@@ -2,35 +2,35 @@ import React from 'react';
 import { Tooltip, Modal } from 'antd';
 
 const PropsModal = ({ availableProps, propsModal, toggle }) => {
-  return (
-    <Modal
-      visible={propsModal}
-      onCancel={toggle}
-      footer={false}
-      bodyStyle={{
-        maxHeight: '80vh',
-        overflowY: 'scroll',
-        '-webkit-mask-image':
-          'linear-gradient(to bottom, transparent, black 4%, black 96%, transparent)',
-      }}
-      centered={true}
-    >
-      {Object.entries(availableProps).map(([category, props]) => (
-        <>
-          <h4 className="prop-category">{category}</h4>
-          <ul>
-            {Object.entries(props).map(([propName, propData]) => (
-              <li key={propName}>
-                <Tooltip title={propData.description} placement="left">
-                  <span className="prop-name">{propName}</span> :{' '}
-                </Tooltip>
-                <span className="prop-type">{propData.type}</span>
-              </li>
-            ))}
-          </ul>
-        </>
-      ))}
-    </Modal>
-  );
+	return (
+		<Modal
+			visible={propsModal}
+			onCancel={toggle}
+			footer={false}
+			bodyStyle={{
+				maxHeight: '80vh',
+				overflowY: 'scroll',
+				WebkitMaskImage:
+					'linear-gradient(to bottom, transparent, black 4%, black 96%, transparent)',
+			}}
+			centered={true}
+		>
+			{Object.entries(availableProps).map(([category, props]) => (
+				<React.Fragment key={category}>
+					<h4 className="prop-category">{category}</h4>
+					<ul>
+						{Object.entries(props).map(([propName, propData]) => (
+							<li key={propName}>
+								<Tooltip title={propData.description} placement="left">
+									<span className="prop-name">{propName}</span> :{' '}
+								</Tooltip>
+								<span className="prop-type">{propData.type}</span>
+							</li>
+						))}
+					</ul>
+				</React.Fragment>
+			))}
+		</Modal>
+	);
 };
 export default PropsModal;
