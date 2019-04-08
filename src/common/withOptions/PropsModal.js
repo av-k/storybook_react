578deb1ff3ff7303930d716a4c25f45126b936bf
tourervit <tourervit@gmail.com>
2019-04-08 13:45:34 +0700

    Added link to Prop Name

--- src/common/withOptions/PropsModal.js
+++ src/common/withOptions/PropsModal.js
@@ -1,36 +1,42 @@
 import React from 'react';
-import { Tooltip, Modal } from 'antd';
+import { Tooltip, Modal, Icon } from 'antd';
 
-const PropsModal = ({ availableProps, propsModal, toggle }) => {
-	return (
-		<Modal
-			visible={propsModal}
-			onCancel={toggle}
-			footer={false}
-			bodyStyle={{
-				maxHeight: '80vh',
-				overflowY: 'scroll',
-				WebkitMaskImage:
-					'linear-gradient(to bottom, transparent, black 4%, black 96%, transparent)',
-			}}
-			centered={true}
-		>
-			{Object.entries(availableProps).map(([category, props]) => (
-				<React.Fragment key={category}>
-					<h4 className="prop-category">{category}</h4>
-					<ul>
-						{Object.entries(props).map(([propName, propData]) => (
-							<li key={propName}>
-								<Tooltip title={propData.description} placement="left">
-									<span className="prop-name">{propName}</span> :{' '}
-								</Tooltip>
-								<span className="prop-type">{propData.type}</span>
-							</li>
-						))}
-					</ul>
-				</React.Fragment>
-			))}
-		</Modal>
-	);
+const PropsModal = ({ dataProps, propsModal, toggle }) => {
+  return (
+    <Modal
+      visible={propsModal}
+      onCancel={toggle}
+      footer={false}
+      bodyStyle={{
+        maxHeight: '80vh',
+        overflowY: 'scroll',
+        WebkitMaskImage:
+          'linear-gradient(to bottom, transparent, black 4%, black 96%, transparent)',
+      }}
+      centered={true}
+    >
+      {Object.entries(dataProps).map(([category, [link, props]]) => (
+        <React.Fragment key={category}>
+          <h4 className="prop-category">
+            {category}
+            <a href={link} target="_blank" className="prop-link">
+              <Icon type="ant-design" />
+            </a>
+          </h4>
+
+          <ul>
+            {Object.entries(props).map(([propName, propData]) => (
+              <li key={propName}>
+                <Tooltip title={propData.description} placement="left">
+                  <span className="prop-name">{propName}</span> :{' '}
+                </Tooltip>
+                <span className="prop-type">{propData.type}</span>
+              </li>
+            ))}
+          </ul>
+        </React.Fragment>
+      ))}
+    </Modal>
+  );
 };
 export default PropsModal;
--- src/common/withOptions/withOptions.js
+++ src/common/withOptions/withOptions.js
@@ -3,43 +3,43 @@ import { Button, Icon, ButtonGroup } from 'antd';
 import PropsModal from './PropsModal';
 import HighlightedSourceCode from './HighlightedSourceCode';
 
-export default (Component, availableProps, code) =>
-  class WithOptions extends React.Component {
-    state = {
-      propsModal: false,
-      sourceCode: false,
-    };
-    toggle = value => {
-      this.setState({ [value]: !this.state[value] });
-    };
+export default (Component, dataProps, code) =>
+	class WithOptions extends React.Component {
+		state = {
+			propsModal: false,
+			sourceCode: false,
+		};
+		toggle = value => {
+			this.setState({ [value]: !this.state[value] });
+		};
 
-    render() {
-      const { sourceCode, propsModal } = this.state;
-      return (
-        <>
-          <Component />
-          <section className="options">
-            <Button.Group>
-              <Button
-                onClick={() => this.toggle('propsModal')}
-                icon="setting"
-              />
-              <Button
-                onClick={() => this.toggle('sourceCode')}
-                type="primary"
-                icon="code"
-              />
-            </Button.Group>
+		render() {
+			const { sourceCode, propsModal } = this.state;
+			return (
+				<>
+					<Component />
+					<section className="options">
+						<Button.Group>
+							<Button
+								onClick={() => this.toggle('propsModal')}
+								icon="setting"
+							/>
+							<Button
+								onClick={() => this.toggle('sourceCode')}
+								type="primary"
+								icon="code"
+							/>
+						</Button.Group>
 
-            <PropsModal
-              availableProps={availableProps}
-              propsModal={propsModal}
-              toggle={() => this.toggle('propsModal')}
-            />
+						<PropsModal
+							dataProps={dataProps}
+							propsModal={propsModal}
+							toggle={() => this.toggle('propsModal')}
+						/>
 
-            {sourceCode && <HighlightedSourceCode code={code} />}
-          </section>
-        </>
-      );
-    }
-  };
+						{sourceCode && <HighlightedSourceCode code={code} />}
+					</section>
+				</>
+			);
+		}
+	};
